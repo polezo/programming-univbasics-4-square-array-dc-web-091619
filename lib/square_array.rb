3e4ebf5e90fba479a24f841d5ecbaf17c742f970
Adam Finch <atyreefinch@gmail.com>
2019-08-04 02:30:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
+  new_arr = []
   i = 0
-  while array.length > i do
-    array[i]**2
+  while i < array.length do
+    new_arr << (array[i] ** 2)
+    i += 1
+  end
+  new_arr
 end
